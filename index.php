57b016884172cfd0c1ff8c0757d4d89d2629c85c
marcosheiner <marcosheiner2000@gmail.com>
2021-04-23 19:08:28 -0300

    index add

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include 'includes/menu.php' ?>
+    
+    
+<div class="background-home">
+    <div class="container">
+        <div class="flexbox">
+            <div class="flex-item shadow">
+                <h2 class="title-login">Hire.</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="">E-mail:</label>
+                        <input type="email" class="form-control" name="" aria-describedby="emailHelp">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Senha:</label>
+                        <input type="password" class="form-control" name="">
+                    </div>
+                    
+                    <input type="submit" class="btn btn-login" value="Entrar"></input>
+                </form>
+                <br>
+                <div class="text-center">
+                    <a href="../pages/cadastrar.php">Cadastre-se</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="footer-home fixed-bottom">
+    <div class="text-center">
+        <small>Copyright ® 2021 Hire Todos os direitos reservados</small>
+    </div>
+</footer>
+    
+    
+<?php include 'includes/footer.php' ?>
